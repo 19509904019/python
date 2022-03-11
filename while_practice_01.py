a488561fc68f8c981c978691e8d2e8e8a530e6a7
yujun <yujunaddress@163.com>
2022-03-11 16:22:10 +0800

    master

---
+++ while_practice_01.py
@@ -0,0 +1,19 @@
+"""
+
+超市购物付款
+
+"""
+
+total = 0
+sum = 0
+while True:
+    price = float(input("请输入商品的价格："))
+    number = int(input("请输入商品的数量："))
+
+    total += price * number
+    sum += number
+    answer = input("当前的商品总额为%.2f,是否退出当前结算页面(按“q”键结束,”s“键继续):" % total)
+    if answer == 'q':
+        break
+
+print("共买了%d件商品，商品的总价格为%.2f" % (sum, total))
---
+++ while_practice_03.py
@@ -0,0 +1,37 @@
+"""
+猜拳游戏： 3局
+剪刀：0  石头：1  布：2
+三局两胜
+
+"""
+
+import random
+
+# 计数
+c_count = 0  # 计算机赢的次数
+p_count = 0  # 人赢的次数
+count = 0 #猜拳次数
+while count <= 2:
+    # 猜拳
+    computer = random.randint(0, 2)
+    guess = int(input("请输入剪刀（0） 石头（1） 布（2）："))  # 输入你想出的数
+    count += 1
+
+    # 比胜负
+    if (computer == 2 and guess == 0) or (computer == 2 and guess == 1) or (computer == 1 and guess == 0):
+        print("很遗憾，您输了...")
+        c_count += 1
+    elif (computer == 0 and guess == 2) or (computer == 1 and guess == 2) or (computer == 0 and guess == 1):
+        print("太棒了，您赢了...")
+        p_count += 1
+    else:
+        print("打平了...")
+
+print()
+# 判断结果
+if c_count > p_count:
+    print("最终您输了")
+elif c_count < p_count:
+    print("最终您赢了")
+else:
+    print("最终打平了")
