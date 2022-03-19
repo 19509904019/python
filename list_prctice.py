992fa827c83fcd295521662fb6ca915bfad7d8a5
yujun <yujunaddress@163.com>
2022-03-19 11:33:52 +0800

    master

--- list_prctice.py
+++ list_prctice.py
@@ -38,24 +38,29 @@
 生成8个1-100之间的随机整数，保存到列表中，
 键盘输入一个1-100之间的整数，将整数插入到排序后的列表中（升降没有要求）
 '''
-import random
-# 保存随机整数
-numbers = []
-# 随机取数
-numbers = random.sample(range(1, 100), 8)
-# 对数据进行排序
-numbers.sort()
-# 打印数据
-print("随机成的数据为：{}".format(numbers))
-while True:
-    number = int(input("请输入一个1-100之间的整数："))
-    # 将数据在列表当中排序
-    if 1<=number<=100:
-        #添加到列表当中
-        numbers.append(number)
-        #重新排序
-        numbers.sort()
-        print("重新排序的列表为：{}".format(numbers))
-        break
-    else:
-        print("输入错误，请重新输入!")
+# import random
+# # 保存随机整数
+# numbers = []
+# # 随机取数
+# numbers = random.sample(range(1, 100), 8)
+# # 对数据进行排序
+# numbers.sort()
+# # 打印数据
+# print("随机成的数据为：{}".format(numbers))
+# while True:
+#     number = int(input("请输入一个1-100之间的整数："))
+#     # 将数据在列表当中排序
+#     if 1<=number<=100:
+#         #添加到列表当中
+#         numbers.append(number)
+#         #重新排序
+#         numbers.sort()
+#         print("重新排序的列表为：{}".format(numbers))
+#         break
+#     else:
+#         print("输入错误，请重新输入!")
+
+
+'''
+冒泡排序
+'''
