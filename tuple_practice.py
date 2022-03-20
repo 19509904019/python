8b8246846a63569373e750adeb8506fd104be4d1
yujun <yujunaddress@163.com>
2022-03-20 23:57:44 +0800

    master

--- tuple_practice.py
+++ tuple_practice.py
@@ -115,3 +115,74 @@
 
 
 
+'''
+王者荣耀角色管理
+角色：姓名   性别   职业
+
+1.添加角色
+2.删除角色
+3.修改角色
+4.查询角色  单个角色
+5.显示所有角色
+6.退出系统
+'''
+import time 
+print("----------欢迎进入王者荣耀管理系统----------")
+#存放所有角色信息
+all_role = []
+while True:
+    #选择操作
+    choice = input("\n1.添加角色\n2.删除角色\n3.修改角色\n\
+4.查询单个角色\n5.查询所有角色\n6.退出系统\n请选择操作：")
+    #添加角色
+    if choice == '1':
+        while True:
+            name = input("请输入角色名称：")
+            gender = input("请输入角色性别：")
+            career = input("请输入角色职业：")
+            #保存角色信息
+            role = [name,gender,career]
+            all_role.append(role)
+            print("角色{}添加成功！".format(name))
+            answer = input("是否继续添加(按q或者Q结束)：")
+            if answer.lower() == 'q':
+                print("退出成功！")
+                break
+
+    elif choice == '2':
+        pass
+    elif choice == '3':
+        pass
+    #查询单个角色
+    elif choice == '4':
+        flag = True
+        while flag:
+            if len(all_role) != 0:
+                while flag:
+                    select_name = input("请输入查询的角色名称：")
+                    for role in all_role:
+                        if select_name in role:
+                            print("{}{}{}".format('姓名'.ljust(5),'性别'.ljust(5),'职业'.ljust(5)))
+                            print("{}{}{}".format(role[0].ljust(5),role[1].ljust(6),role[2].ljust(5)))
+                            break
+                    else:
+                        print("角色不存在！")
+                    answer = input("是否继续查询(按q或者Q结束)：")
+                    if answer.lower() == 'q':
+                        print("退出成功！")
+                        flag = False
+                        break
+            else:
+                print("角色列表为空！")
+                break
+
+    elif choice == '5':
+        pass
+    elif choice == '6':
+        print("正在退出系统...")
+        time.sleep(2)
+        print("退出成功！")
+        break
+    else:
+        print("输入错误，请重新输入！")
+    
