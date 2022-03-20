6b7b7aea9df8897a61cfc7c9c6afb87237e49030
yujun <yujunaddress@163.com>
2022-03-20 21:10:15 +0800

    master

--- dict_operation.py
+++ dict_operation.py
@@ -24,6 +24,49 @@ dict['gender'] = '女'
 dict['age'] = 18
 print(dict)
 
-#改变年龄   键是不能修改的，可以添加和删除，但后面的值可以修改
-dict['age'] = 17
-print(dict)
+# 改变年龄   键是不能修改的，可以添加和删除，但后面的值可以修改
+# dict['age'] = 17
+# print(dict)
+
+''''
+字典删除：
+pop(key):删除的是键值对，返回值key对应的value
+popitem():返回值(key,value)  从后往前删内容
+'''
+# dict.pop('age')
+# print(dict)
+
+# a = dict.popitem()
+# print(a)
+# print(dict)
+
+
+'''
+遍历和查询：
+列表：list.index()   list.count()    in 
+
+获取：
+dict.get(key)  根据key获取value值
+dict[key]
+区别：get(key)里面的key如果不存在则返回None，同时get(key,默认值)可以设置默认值
+     dict[key] 会报error错误
+
+
+如果使用for...in...直接遍历字典，取出的是字典的key
+for i in dict:
+    print(i)
+
+获取字典中所有的key值：字典.keys()
+获取字典中所有的value值：字典.values()
+
+'''
+#根据key得到value值  get不会报错
+# value = dict.get('age')
+# print(value)
+
+
+for i in dict.keys():
+    print(i)
+
+for i in dict.values():
+    print(i)
