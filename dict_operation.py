b73ebcb9f7e74b27744db41d7880decea353ee06
yujun <yujunaddress@163.com>
2022-03-20 20:08:51 +0800

    master

--- dict_operation.py
+++ dict_operation.py
@@ -10,12 +10,20 @@
 字典名[key] = value
 注意：key是唯一的，所以在添加的时候如果出现同名的key,后面key对应的value则替换原来的
 
+2.修改value值
+字典名[key] = value
 
+关键看key:如果字典中不存在键，则是添加
+         如果字典中存在键，则是修改
 '''
-dict1 = {}
-print(type(dict1))
+dict = {}
+print(type(dict))
 
-dict1['name'] = '阿燕'
-dict1['gender'] = '女'
-dict1['age'] = 18
-print(dict1)
+dict['name'] = '阿燕'
+dict['gender'] = '女'
+dict['age'] = 18
+print(dict)
+
+#改变年龄   键是不能修改的，可以添加和删除，但后面的值可以修改
+dict['age'] = 17
+print(dict)
