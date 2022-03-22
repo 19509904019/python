a3c1e32a41d85b3759e9d87e1cc96d429f755f1c
yujun <yujunaddress@163.com>
2022-03-23 00:35:09 +0800

    master

--- set/set_operation.py
+++ set/set_operation.py
@@ -22,11 +22,47 @@ add  update  ----->set
 list:有序，允许重复
 set:无序，不允许重复
 '''
-# 添加元素  add
-set1 = set()
-set1.add("三体")
-print(set1)
+# # 添加元素  add
+# set1 = set()
+# set1.add("三体")
+# print(set1)
 
-result = set1.add("三体")
-print(set1)
+# result = set1.add("三体")
+# print(set1)
+# print(result)
+
+
+'''
+删除：remove()   discard()
+del set : 删除整个集合
+pop():随机删除集合中任意一个元素
+'''
+
+# set1 = {'1','2','3','4','5'}
+# print(set1)
+
+# set1.remove('1')
+# print(set1)
+# # set1.remove('6')  #报错  keyErrorr
+# # print(set1)
+
+# set1.discard('2')  
+# print(set1)
+# set1.discard('2')  #没有元素时，不报错
+# print(set1)
+
+'''
+交集(&)：intersection
+并集(|)：union
+差集(-)：difference
+'''
+
+set1 = {0,1,2,3,4,5}
+set2 = {4,5,6,7,8,9}
+
+result = set1.difference(set2)
+print(result)
+result = set1.union(set2)
+print(result)
+result = set1.intersection(set2)
 print(result)
