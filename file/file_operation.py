72d036bf6b25dce54bf14a19314ea12ae6602374
yujun <yujunaddress@163.com>
2022-03-26 21:57:58 +0800

    master

---
+++ file/file_operation.py
@@ -0,0 +1,27 @@
+'''
+Description: 文件操作
+version: 
+Author: yujun
+Date: 2022-03-26 21:28:47
+LastEditors: yujun
+LastEditTime: 2022-03-26 21:57:08
+'''
+
+'''
+文件上传：
+保存log
+
+系统函数：
+open()
+
+mode: r  w  rb   wb
+
+r, w: 纯文本文件
+rb,wb:纯文本  图片  音乐  视频
+
+r:输入
+w:输出
+
+缓存：buffering  调节硬件和CPU之间的缓冲
+编码：encoding
+'''
