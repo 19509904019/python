c89dfb375084dd3603f11f1c1583112ec35b58bb
yujun <yujunaddress@163.com>
2022-03-27 11:13:20 +0800

    master

--- file/file_operation.py
+++ file/file_operation.py
@@ -4,7 +4,7 @@ version:
 Author: yujun
 Date: 2022-03-26 21:28:47
 LastEditors: yujun
-LastEditTime: 2022-03-26 21:57:08
+LastEditTime: 2022-03-27 11:12:27
 '''
 
 '''
@@ -12,7 +12,7 @@ LastEditTime: 2022-03-26 21:57:08
 保存log
 
 系统函数：
-open()
+open(file,mode,buffering,encoding)
 
 mode: r  w  rb   wb
 
@@ -25,3 +25,33 @@ w:输出
 缓存：buffering  调节硬件和CPU之间的缓冲
 编码：encoding
 '''
+'''
+读：
+    open(path/filename,'rt')  -----> 返回值：stream(管道)
+    stream.read()  -----> 读取管道中的内容
+注意：如果传递的path/filename有误，则会报错：FileNotFoundError
+      如果是图片则不能使用默认的读取方式，mode = 'rb'
+
+总结：
+    read() : 读取文件
+    readline() : 每次读取一行内容
+    readlines(): 读取所有的行保存到列表中
+    readable() : 判断是否可读
+'''
+stream = open(r'C:\Users\12414\Desktop\score.txt',encoding='UTF-8')  #当成是一个管道，暂存文件的内容
+# container = stream.read()
+# print(container) 
+
+
+result = stream.readable()  #判断文件是否可读 True False
+print(result)
+
+# while True:
+#     result = stream.readline()  #按行来输出
+#     print(result)
+#     if not result:
+#         break
+
+result = stream.readlines()
+for i in result:
+    print(i)
