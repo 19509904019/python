b36c3a0cdbc1fbc49850c2bd34a02d89246c8b06
yujun <yujunaddress@163.com>
2022-04-13 11:49:37 +0800

    Changes

--- process/process01.py
+++ process/process01.py
@@ -11,7 +11,7 @@
 
 并发和并行
 并发：当有多个线程在操作时，如果系统只有一个CPU，则它根本不可能真正同时进行一个以上的线程，它只能把CPU运行时间划分成若干个时间时间段，再将时间
-段分配给各个线程执行，在一个时间段的线程代码运行时，其它线程处于挂起状态
+段分配给各个线程执行，在一个时间段的线程代码运行时，其它线程处于挂起状态,这种称为并发
 
 并行：当系统有一个以上CPU时，则线程的擦作有可能并发。当一个CPU执行一个线程时，另一个CPU可以执行另一个线程，两个线程互不抢占CPU资源，可以同时进行，
 这种方式称为并行
@@ -21,4 +21,34 @@
 2.多线程模式
 3.协程
 
+进程 > 线程 > 协程
+
+进程
+优点：稳定性高，一个进程崩溃了不会影响其他进程
+缺点：①创建进程开销巨大  ②操作系统能同时运行的进程数目是有限的
 """
+# 进程创建
+from multiprocessing import Process
+from time import sleep
+
+
+def task1():
+    while True:
+        sleep(1)
+        print("这是任务1....")
+
+
+def task2():
+    while True:
+        sleep(1)
+        print("这是任务2....")
+
+
+if __name__ == '__main__':
+    #子进程
+    p1 = Process(target=task1,name="任务1")
+    p1.start()
+    print(p1.name)
+    p2 = Process(target=task2,name="任务2")
+    p2.run()
+    print(p2.name)
